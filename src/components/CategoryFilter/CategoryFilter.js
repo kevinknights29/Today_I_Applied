29de17d93b549c00bdbbc643b18fffe7bfa37c42
Kevin Knights <kevinknights29@hotmail.com>
2023-11-13 16:10:59 -0500

    added CategoryFilter component

---
+++ src/components/CategoryFilter/CategoryFilter.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const CategoryFilter = () => {
+  return (
+    <div>
+      <aside class="sidebar">
+        <ul>
+          <li>
+            <button>All</button>
+          </li>
+          <li>
+            <button>Engineering</button>
+          </li>
+          <li>
+            <button>Data</button>
+          </li>
+        </ul>
+      </aside>
+    </div>
+  );
+};
+
+export default CategoryFilter;
