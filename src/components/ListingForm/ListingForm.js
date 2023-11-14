832a2a139a2e1d74afc3a675a885f8252f6d53c3
Kevin Knights <kevinknights29@hotmail.com>
2023-11-14 17:30:59 -0500

    added character counter for listing form inputs

--- src/components/ListingForm/ListingForm.js
+++ src/components/ListingForm/ListingForm.js
@@ -1,23 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ListingForm = () => {
+  const [roleName, setRole] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [applicationUrl, setApplicationUrl] = useState("");
+
+  const handleRoleNameChange = (event) => {
+    setRole(event.target.value);
+  };
+  const handleCompanyNameChange = (event) => {
+    setCompanyName(event.target.value);
+  };
+  const handleApplicationUrlChange = (event) => {
+    setApplicationUrl(event.target.value);
+  };
+
   return (
     <div className="job-form" id="job-form-div">
       <form action="" method="">
         <label htmlFor="role">Role:</label>
-        <input type="text" id="role" name="role" required />
-        <span>200</span>
+        <input
+          type="text"
+          id="role"
+          name="role"
+          value={roleName}
+          onChange={handleRoleNameChange}
+          required
+        />
+        <span>{200 - roleName.length}</span>
         <label htmlFor="company">Company:</label>
-        <input type="text" id="company" name="company" required />
-        <span>200</span>
+        <input
+          type="text"
+          id="company"
+          name="company"
+          value={companyName}
+          onChange={handleCompanyNameChange}
+          required
+        />
+        <span>{200 - companyName.length}</span>
         <label htmlFor="application-url">Application URL:</label>
         <input
           type="url"
           id="application-url"
           name="application-url"
+          value={applicationUrl}
+          onChange={handleApplicationUrlChange}
           required
         />
-        <span>200</span>
+        <span>{200 - applicationUrl.length}</span>
         <label htmlFor="location">Location:</label>
         <select name="" id="" required>
           <option value="">Panama City, Panama</option>
