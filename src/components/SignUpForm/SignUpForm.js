9a39204cdd83b883871a8af0b6131defde9c67f5
Kevin Knights <kevinknights29@hotmail.com>
2023-11-13 23:10:00 -0500

    added authenticated signup

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -1,26 +1,40 @@
+import { createClient } from "@supabase/supabase-js";
 import React, { useState } from "react";
 
 function SignUpForm(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [name, setName] = useState("");
-  const handleSubmit = (event) => {
+  const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+  const supabaseKey = process.env.REACT_APP_SUPABASE_API_KEY;
+  /**
+   * Handles form submission by inserting user data into a Supabase database.
+   * @param {Event} event - The form submission event.
+   */
+  const handleSubmit = async (event) => {
+    // Prevent the default form behavior
     event.preventDefault();
     console.log(`Email: ${email}, Password: ${password}`);
+
+    // Create a single supabase client for interacting with your database
+    console.log(supabaseUrl, supabaseKey);
+    const supabase = createClient(supabaseUrl, supabaseKey);
+
+    // Insert a new user into your table
+    const { data, error } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+    });
+
+    if (error) {
+      console.error(error);
+    } else {
+      console.log("User inserted successfully:", data);
+    }
   };
 
   return (
     <div className="sign-up-form">
       <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Full Name:</label>
-        <input
-          id="name"
-          name="name"
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="John Doe"
-        />
         <label htmlFor="email">Email:</label>
         <input
           id="email"
