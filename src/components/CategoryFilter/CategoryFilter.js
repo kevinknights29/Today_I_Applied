f4daed14b55f279d226ca7e52860d20fc5fb6f30
Kevin Knights <kevinknights29@hotmail.com>
2023-11-15 00:29:05 -0500

    feat: dynamically list categories for filtering

--- src/components/CategoryFilter/CategoryFilter.js
+++ src/components/CategoryFilter/CategoryFilter.js
@@ -1,19 +1,39 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import supabase from "../../client/supabaseClient";
+import CategoryCard from "../CategoryCard/CategoryCard";
 
 const CategoryFilter = () => {
+  // Define state for the categories
+  const [categories, setCategories] = useState([]);
+
+  // Fetch the job categories (tags) from the database when the component mounts
+  useEffect(() => {
+    const fetchCategories = async () => {
+      let { data: categories, error } = await supabase
+        .from("categories")
+        .select("id, category")
+        .range(0, 9);
+
+      if (error) {
+        console.error(error);
+      } else {
+        console.log("Read job categories successfully:", categories);
+        setCategories(categories);
+      }
+    };
+    fetchCategories();
+  }, []);
+
   return (
     <div>
       <aside className="sidebar">
         <ul>
-          <li>
-            <button>All</button>
-          </li>
-          <li>
-            <button>Engineering</button>
-          </li>
-          <li>
-            <button>Data</button>
-          </li>
+          {categories.map((category) => (
+            <li key={category.id}>
+              <CategoryCard category={category} />
+            </li>
+          ))}
         </ul>
       </aside>
     </div>
