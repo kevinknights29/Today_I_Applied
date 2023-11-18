a4020ce817dd32636ccda99b7364602fd49ac328
Kevin Knights <kevinknights29@hotmail.com>
2023-11-18 10:25:14 -0500

    WIP: adding update or insert reactions

--- src/components/ListingCard/ListingCard.js
+++ src/components/ListingCard/ListingCard.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import supabase from "../../client/supabaseClient";
 
 const ListingCard = (prop) => {
   const {
+    id,
     role: roleName,
     company: companyName,
     url: applicationUrl,
@@ -9,10 +11,91 @@ const ListingCard = (prop) => {
     tags,
   } = prop.job;
 
-  const likes = 0;
-  const redFlags = 0;
-  const applications = 0;
-  const comments = 0;
+  const [likes, setLikes] = useState(0);
+  const [redFlags, setRedFlags] = useState(0);
+  const [applications, setApplications] = useState(0);
+  const [comments, setComments] = useState(0);
+
+  const handleReaction = async (type) => {
+    // Get Authenticated User ID
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    console.log(user.id);
+
+    let { data: reaction, error } = await supabase
+      .from("reactions")
+      .select("id")
+      .eq("job_id", id)
+      .eq("user_id", user.id);
+    console.log("Retrived reaction:", reaction);
+
+    if (error) {
+      console.error(error);
+    } else if (reaction.length === 0) {
+      let { data: r, error } = await supabase
+        .from("reactions")
+        .insert({ job_id: id, user_id: user.id, type: type }, { upsert: true })
+        .select();
+      if (error) {
+        console.error(error);
+      } else {
+        console.log("Insert reaction successfully:", r);
+      }
+    } else {
+      let { data: r, error } = await supabase
+        .from("reactions")
+        .update({ type: type })
+        .eq("id", reaction[0].id)
+        .select();
+      if (error) {
+        console.error(error);
+      } else {
+        console.log(
+          `Update reaction_id ${reaction[0].id} with ${type} successfully:, `
+        );
+        console.log("Update reaction successfully:", r);
+      }
+    }
+  };
+
+  // Fetch reactions from the database when the component mounts
+  useEffect(() => {
+    const fetchReactions = async () => {
+      // Get Authenticated User ID
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      console.log(user.id);
+
+      let { data: count_likes, error_likes } = await supabase
+        .from("reactions")
+        .select("*", { count: "exact", head: true })
+        .eq("job_id", id)
+        .eq("type", "like");
+
+      if (error_likes) {
+        console.error(error_likes);
+      } else {
+        console.log("Read likes successfully:", count_likes);
+        setLikes(count_likes === null ? 0 : count_likes);
+      }
+
+      let { data: count_redFlags, error_redFlags } = await supabase
+        .from("reactions")
+        .select("*", { count: "exact", head: true })
+        .eq("job_id", id)
+        .eq("type", "dislike");
+
+      if (error_redFlags) {
+        console.error(error_redFlags);
+      } else {
+        console.log("Read redFlags successfully:", count_redFlags);
+        setRedFlags(count_redFlags === null ? 0 : count_redFlags);
+      }
+    };
+    fetchReactions();
+  }, []);
 
   const openLink = (link) => {
     window.open(link, "_blank");
@@ -45,8 +128,10 @@ const ListingCard = (prop) => {
         <span>#{tags[0]}#</span>
       </div>
       <div className="reactions">
-        <button>👍 {likes}</button>
-        <button>🚩 {redFlags}</button>
+        <button onClick={() => handleReaction("like")}>👍 {likes}</button>
+        <button onClick={() => handleReaction("red_flag")}>
+          🚩 {redFlags}
+        </button>
         <button>✅ {applications}</button>
         <button>💬 {comments}</button>
       </div>
