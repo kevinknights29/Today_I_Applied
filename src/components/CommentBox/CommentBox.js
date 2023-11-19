c19f2b50578a052a6f08bc4e39465e8b996a6657
Kevin Knights <kevinknights29@hotmail.com>
2023-11-19 01:42:44 -0500

    completed CommentBox component

--- src/components/CommentBox/CommentBox.js
+++ src/components/CommentBox/CommentBox.js
@@ -1,13 +1,44 @@
 import React, { useState } from "react";
+import supabase from "../../client/supabaseClient";
 
-const CommentBox = () => {
+const CommentBox = ({ jobID }) => {
   const [comment, setComment] = useState("");
   const handleCommentChange = (event) => {
     setComment(event.target.value);
   };
 
+  const handleSubmit = async (event) => {
+    // Prevent the default form behavior
+    event.preventDefault();
+    console.log(`Comment: ${comment}`);
+
+    // Get Authenticated User ID
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    console.log(user.id);
+
+    // Insert a new comment
+    const { data, error } = await supabase
+      .from("comments")
+      .insert([
+        {
+          user_id: user.id,
+          job_id: jobID,
+          content: comment,
+        },
+      ])
+      .select();
+
+    if (error) {
+      console.error(error);
+    } else {
+      console.log("Comment inserted successfully:", data);
+    }
+  };
+
   return (
-    <form className="comment-box">
+    <form className="comment-box" onSubmit={(event) => handleSubmit(event)}>
       <input
         id="comment"
         name="comment"
