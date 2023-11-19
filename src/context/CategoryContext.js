b2697c417a5fcc077d9f5ac92719be73b7e8adf4
Kevin Knights <kevinknights29@hotmail.com>
2023-11-19 13:49:13 -0500

    feat: added context for category filtering

---
+++ src/context/CategoryContext.js
@@ -0,0 +1,21 @@
+import React, { createContext, useState, useContext } from "react";
+
+const CategoryContext = createContext();
+
+export const useCategory = () => useContext(CategoryContext);
+
+export const CategoryProvider = ({ children }) => {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category);
+  };
+
+  return (
+    <CategoryContext.Provider
+      value={{ selectedCategory, handleCategoryChange }}
+    >
+      {children}
+    </CategoryContext.Provider>
+  );
+};
