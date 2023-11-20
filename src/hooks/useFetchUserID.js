8844343e7d99fe365136214b75cb584b6ee51e03
Kevin Knights <kevinknights29@hotmail.com>
2023-11-19 19:52:19 -0500

    feat: added hook to fetch user id

---
+++ src/hooks/useFetchUserID.js
@@ -0,0 +1,19 @@
+import {useState, useEffect} from 'react';
+import {getCurrentUserId} from './client/supabaseAuth';
+
+const useFetchUserID = () => {
+  const [userID, setUserID] = useState(null);
+
+  useEffect(() => {
+    const fetchUserID = async () => {
+      const id = await getCurrentUserId();
+      setUserID(id);
+    };
+
+    fetchUserID();
+  }, []);
+
+  return userID;
+};
+
+export default useFetchUserID;
