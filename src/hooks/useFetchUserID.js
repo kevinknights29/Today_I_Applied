5c3975d37cb54c68c50bc98cec774948ded7609f
Kevin Knights <kevinknights29@hotmail.com>
2023-11-19 19:55:08 -0500

    fix: typo in import

--- src/hooks/useFetchUserID.js
+++ src/hooks/useFetchUserID.js
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react';
-import {getCurrentUserId} from './client/supabaseAuth';
+import {getCurrentUserId} from '../client/supabaseAuth';
 
 const useFetchUserID = () => {
   const [userID, setUserID] = useState(null);
