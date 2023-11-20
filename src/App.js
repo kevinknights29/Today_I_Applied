2dcec30c7f033e3f38f9e3e60a2ff3858dc904e7
Kevin Knights <kevinknights29@hotmail.com>
2023-11-19 19:14:52 -0500

    fix: solved eslint error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import Login from './components/Login/Login';
 import Header from './components/Header/Header';
 import CategoryFilter from './components/CategoryFilter/CategoryFilter';
@@ -6,6 +7,14 @@ import Listing from './components/Listing/Listing';
 import Footer from './components/Footer/Footer';
 import {CategoryProvider} from './context/CategoryContext';
 
+/**
+ * App component is the root of the application.
+ * It wraps the entire application with the CategoryProvider
+ * for state management and includes major components like:
+ * Login, Header, CategoryFilter, Listing, and Footer.
+ *
+ * @return {JSX.Element} The App component rendered as a React element.
+ */
 function App() {
   return (
     <div className="App">
